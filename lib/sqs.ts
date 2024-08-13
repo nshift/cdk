35a583e0498059572f613821ddfebc2520747f78
Romain Asnar <romain.asnar@gmail.com>
2024-08-13 16:57:05 +0700

    Improve naming for SQS.

--- lib/sqs.ts
+++ lib/sqs.ts
@@ -1,12 +1,13 @@
 import * as cdk from 'aws-cdk-lib'
+import { makeId, makeName } from './naming'
 
 export const createSQS = (name: string, props: { stack: cdk.Stack }) => {
-  const deadLetterQueue = new cdk.aws_sqs.Queue(props.stack, `${name}DeadLetterQueue`, {
-    queueName: `${name}DeadLetterQueue`,
+  const deadLetterQueue = new cdk.aws_sqs.Queue(props.stack, makeId(`${name}DeadLetterQueue`), {
+    queueName: makeName(`${name}DeadLetterQueue`),
     retentionPeriod: cdk.Duration.days(7),
   })
-  return new cdk.aws_sqs.Queue(props.stack, `${name}Queue`, {
-    queueName: `${name}Queue`,
+  return new cdk.aws_sqs.Queue(props.stack, makeId(`${name}Queue`), {
+    queueName: makeName(`${name}Queue`),
     visibilityTimeout: cdk.Duration.seconds(30),
     deadLetterQueue: {
       maxReceiveCount: 1,
